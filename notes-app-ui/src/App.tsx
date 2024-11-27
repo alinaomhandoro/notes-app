19d0dbe89f297405f62f0a366b3eefd6a97b5ef1
Alina Omhandoro <alina.omhandoro@gmail.com>
2024-11-27 20:54:22 +0000

    notes mapped to components, state used for form inputs, function that handles the form submission added, add note functionality added, new note object added, state updated with new note, form inputs cleared, update note functionality added, handle note click functionality added

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,25 +1,185 @@
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="app-container">
-      <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
 
+const App = () => {
+  const [notes, setNotes] = useState
+  <Note[]
+  >([
+    {
+      id: 1,
+      title: "Note 1",
+      content: "I",
+    },
+    {
+      id: 2,
+      title: "Note 2",
+      content: "want",
+    },
+    {
+      id: 3,
+      title: "Note 3",
+      content: "to",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "thank",
+    },
+    {
+      id: 5,
+      title: "Note 5",
+      content: "me",
+    },
+    {
+      id: 6,
+      title: "Note 6",
+      content: "for",
+    },
+    {
+      id: 7,
+      title: "Note 7",
+      content: "believing",
+    },
+    {
+      id: 8,
+      title: "Note 8",
+      content: "in",
+    },
+    {
+      id: 9,
+      title: "Note 9",
+      content: "me.",
+    },
+    {
+      id: 10,
+      title: "Note 10",
+      content: "That is all!",
+    },
+    ]);
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const [selectedNote, setSelectedNote] =
+      useState<Note | null>(null);
+
+    const handleNoteClick = (note:Note) => {
+      setSelectedNote(note);
+      setTitle(note.title);
+      setContent(note.content);
+    }
+
+    const handleAddNote = (
+      event: React.FormEvent
+    ) => {
+      event.preventDefault();
+
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const newNote: Note = {
+        id: notes.length + 1,
+        title: title,
+        content: content,
+      }
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setContent("");
+    };
+
+    const handleUpdateNote = (
+      event: React.FormEvent
+    ) => {
+      event.preventDefault();
+
+      if(!selectedNote) {
+        return;
+      }
+
+      const updatedNote: Note = {
+        id: selectedNote.id,
+        title: title,
+        content: content,
+      }
+
+      const updatedNotesList = notes.map((note)=>
+      note.id === selectedNote.id
+      ? updatedNote
+      : note
+      )
+
+      setNotes(updatedNotesList)
+      setTitle("")
+      setContent("")
+      setSelectedNote(null);
+    };
+
+    const handleCancel = () => {
+      setTitle("")
+      setContent("")
+      setSelectedNote(null);
+    }
+
+  return (
+      <div className="app-container">
+      <form className="note-form"
+            onSubmit={(event) =>
+              selectedNote
+              ? handleUpdateNote(event)
+              : handleAddNote(event)
+            }
+      >
+
+      <input
+        value={title}
+        onChange={(event) =>
+        setTitle(event.target.value)
+        }
+        placeholder="Title"
+        required
+        ></input>
+        <textarea
+        value={content}
+        onChange={(event) =>
+          setContent(event.target.value)
+        }
+        placeholder="Content"
+        rows={10}
+        required
+        ></textarea>
+
+        {selectedNote ? (
+          <div className="edit-buttons">
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+          ) : (
         <button type="submit">Add Note</button>
+          )}
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item"
+          onClick={()=> handleNoteClick(note)}
+          >
+            <div className="notes-header">
+              <button>x</button>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
         </div>
+      ))}
       </div>
     </div>
   );
-};
 
+};
 export default App;
