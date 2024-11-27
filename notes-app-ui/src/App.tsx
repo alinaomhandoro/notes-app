69ed9701914cc0992cdcf94400e925d62343eaeb
Alina Omhandoro <alina.omhandoro@gmail.com>
2024-11-27 21:31:44 +0000

    JSX updated based on the note action, delete note function added

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -124,7 +124,19 @@ const App = () => {
       setTitle("")
       setContent("")
       setSelectedNote(null);
-    }
+    };
+
+    const deleteNote = (
+      event: React.MouseEvent,
+      noteId: number
+    ) => {
+      event.stopPropagation();
+
+      const updatedNotes = notes.filter(
+        (note)=> note.id !== noteId
+      )
+      setNotes(updatedNotes);
+    };
 
   return (
       <div className="app-container">
@@ -171,7 +183,12 @@ const App = () => {
           onClick={()=> handleNoteClick(note)}
           >
             <div className="notes-header">
-              <button>x</button>
+              <button onClick={(event) =>
+                deleteNote(event, note.id)
+              }
+              >
+                x
+                </button>
           </div>
           <h2>{note.title}</h2>
           <p>{note.content}</p>
